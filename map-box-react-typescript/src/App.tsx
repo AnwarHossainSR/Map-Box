9bbabea789dc5b1f91c47446d32b24d7eeec09c0
AnwarHossainSR <mahedianwar@gmail.com>
2021-12-01 10:24:10 +0600

    map box marker

--- map-box-react-typescript/src/App.tsx
+++ map-box-react-typescript/src/App.tsx
@@ -1,23 +1,47 @@
-import React, { useState } from "react";
-import ReactMapGL from "react-map-gl";
+import { useState } from "react";
+import ReactMapGL, { Marker } from "react-map-gl";
+
+interface ViewPort {
+  width: number;
+  height: number;
+  latitude: number;
+  longitude: number;
+  zoom: number;
+  bearing?: number;
+  pitch?: number;
+}
 
 const App = () => {
-  const [viewport, setViewport] = useState({
+  const [viewport, setViewport] = useState<ViewPort>({
     width: window.innerWidth,
     height: window.innerHeight,
-    latitude: 37.78,
-    longitude: -122.45,
-    zoom: 14,
+    latitude: 23.795396,
+    longitude: 90.426012,
+    zoom: 7,
+    bearing: 0,
+    pitch: 0,
   });
-  const accessToken = process.env.REACT_APP_STRING;
-  console.log(process.env.REACT_APP_STRING && process.env.REACT_APP_STRING);
-
+  const accessToken: string | undefined = process.env.REACT_APP_AccessToken;
   return (
     <ReactMapGL
       mapboxApiAccessToken={accessToken}
+      mapStyle="mapbox://styles/mapbox/dark-v9"
       {...viewport}
-      onViewportChange={(viewport: any) => setViewport(viewport)}
-    />
+      onViewportChange={(viewport: ViewPort) => setViewport(viewport)}
+    >
+      <Marker
+        latitude={23.7937}
+        longitude={90.4066}
+        offsetTop={-viewport.zoom * 3}
+      >
+        <img
+          src="https://icons.iconarchive.com/icons/paomedia/small-n-flat/1024/map-marker-icon.png"
+          alt="address"
+          width={viewport.zoom * 3}
+          height={viewport.zoom * 3}
+        /> 
+      </Marker>
+    </ReactMapGL>
   );
 };
 
